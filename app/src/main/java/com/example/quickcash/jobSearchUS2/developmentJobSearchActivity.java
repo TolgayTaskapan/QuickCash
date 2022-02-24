0190060195759bceed25bf05dc8ba9cb87e482c8
Tolgay Taskapan <tl852782@dal.ca>
2022-02-24 18:19:55 -0400

    Added functionality to job search activity

--- app/src/main/java/com/example/quickcash/jobSearchUS2/developmentJobSearchActivity.java
+++ app/src/main/java/com/example/quickcash/jobSearchUS2/developmentJobSearchActivity.java
@@ -1,6 +1,8 @@
 package com.example.quickcash.jobSearchUS2;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +15,14 @@ public class developmentJobSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_development_job_search);
 
+        Spinner spinner = findViewById(R.id.job_type_list);
 
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.job_type_entries,
+                android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
