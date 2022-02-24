f9b230ce41129fdaf53652cd07d4a3191f9078e4
Tolgay Taskapan <tl852782@dal.ca>
2022-02-24 18:40:42 -0400

    Added entries for search categories

--- app/src/main/java/com/example/quickcash/jobSearchUS2/developmentJobSearchActivity.java
+++ app/src/main/java/com/example/quickcash/jobSearchUS2/developmentJobSearchActivity.java
@@ -15,14 +15,33 @@ public class developmentJobSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_development_job_search);
 
-        Spinner spinner = findViewById(R.id.job_type_list);
+        Spinner job_type_spinner = findViewById(R.id.job_type_list);
+        Spinner hourly_wages_spinner = findViewById(R.id.hourly_wages_list);
+        Spinner distance_spinner = findViewById(R.id.distance_list);
+        Spinner job_length_spinner = findViewById(R.id.job_length_list);
 
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.job_type_entries,
+        ArrayAdapter<CharSequence> job_type_adapter = ArrayAdapter.createFromResource(this, R.array.job_type_entries,
                 android.R.layout.simple_spinner_item);
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        job_type_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(adapter);
+        job_type_spinner.setAdapter(job_type_adapter);
+
+        ArrayAdapter<CharSequence> hourly_wages_adapter = ArrayAdapter.createFromResource(this, R.array.hourly_wages_entries,
+                android.R.layout.simple_spinner_item);
+
+        hourly_wages_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        hourly_wages_spinner.setAdapter(hourly_wages_adapter);
+
+        ArrayAdapter<CharSequence> distance_adapter = ArrayAdapter.createFromResource(this, R.array.distance_entries,
+                android.R.layout.simple_spinner_item);
+
+        distance_adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        distance_spinner.setAdapter(distance_adapter);
+
+        
     }
 }
