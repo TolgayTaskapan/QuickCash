84145a609092dcb65db19d194689c7c1db28a9a6
zhutaicheng <tc512621@dal.ca>
2022-03-25 10:59:06 -0300

    us9

--- app/src/main/java/com/example/quickcash/payment/Config.java
+++ app/src/main/java/com/example/quickcash/payment/Config.java
@@ -1,5 +1,5 @@
 package com.example.quickcash.payment;
 
 public class Config {
-    public static final String PAYPAL_CLIENT_ID = "AVHJSVWXVyo71AyKdWHUFwSEMNTAGNtFz2zX3CrFMD1NEPnRYsVWE_lQCAkyP6wnGINQhET_Eq44hcOV";
+    public static final String PAYPAL_CLIENT_ID = "Ab7oXFo7yWYEdz_mQHvPtmPkt_5h2KUSRKjnIPVi0jv9E71YWJfefK-ZMJiICRF5fz2uPDNFSsEOwB2w";
 }
